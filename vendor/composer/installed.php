824c380141f3e1ea9e6f8c87e79ee622e5e75f78
Md. Saiful Islam <saiful.ibne@gmail.com>
2023-07-30 02:19:01 +0600

    add repository link

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'f6b9dedc74cdfbe776deb7540fef13d5b8472adb',
+        'name' => 'mnmsi/gencrudops',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'mnmsi/gencrudops' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'f6b9dedc74cdfbe776deb7540fef13d5b8472adb',
+            'dev_requirement' => false,
+        ),
+    ),
+);
