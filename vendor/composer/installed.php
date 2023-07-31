357da4df010e1a04c022dc6b58f7f56c790fd022
saifuliotait <saiful.iotait@gmail.com>
2023-07-31 15:50:12 +0600

    'commit'

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'mnmsi/gencrudops',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '95b96b2c1a7174eff8683b10bf57b7be175a3010',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'mnmsi/gencrudops' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '95b96b2c1a7174eff8683b10bf57b7be175a3010',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
